4c84505e32136904163d5709b1ff1bb57ccb0ba7
manixcoder <pathakmanish86@gmail.com>
2022-05-03 00:30:47 +0530

    Overriding

---
+++ overridingProperties/index.php
@@ -0,0 +1,24 @@
+<?php
+
+class base
+{
+    public $name = "Parent Class";
+
+    public function calc($a, $b)
+    {
+        return $a * $b;
+    }
+}
+
+class derived extends base
+{
+    public $name = "Child Class";
+    public function calc($a, $b)
+    {
+        return $a + $b;
+    }
+}
+
+$test = new derived();
+
+echo $test->calc(10,55);
